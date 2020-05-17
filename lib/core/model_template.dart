c24fb1e3cade5217b41c07f3c8cb9b21415de495
Mark Mooibroek <mark@appnormal.com>
2020-05-17 21:57:54 +0200

    Added nested model parsing

--- lib/core/model_template.dart
+++ lib/core/model_template.dart
@@ -1,45 +1,68 @@
 import 'package:json_to_model/core/json_model.dart';
 
+typedef JsonModelConverter = String Function(JsonModel data, [bool isNested]);
+
 class ModelTemplates {
-  static String Function(JsonModel) fromJsonModel =
-      (JsonModel data) => ModelTemplates.defaultTemplate(
-            imports: data.imports,
-            fileName: data.fileName,
-            className: data.className,
-            declaration: data.declaration,
-            enums: data.enums,
-            enumConverters: data.enumConverters,
-          );
+  static JsonModelConverter fromJsonModel = (data, [isNested = false]) => ModelTemplates.defaultTemplate(
+        isNested: isNested,
+        imports: data.imports,
+        fileName: data.fileName,
+        className: data.className,
+        declaration: data.declaration,
+        enums: data.enums,
+        enumConverters: data.enumConverters,
+        nestedClasses: data.nestedClasses,
+      );
 
   static String defaultTemplate({
-    imports,
-    fileName,
-    className,
-    declaration,
-    enums,
-    enumConverters,
-  }) =>
-      """
+    bool isNested,
+    String imports,
+    String fileName,
+    String className,
+    String declaration,
+    String enums,
+    String enumConverters,
+    String nestedClasses,
+  }) {
+    var template = '';
+
+    if (!isNested) {
+      template += '''
 import 'package:json_annotation/json_annotation.dart';
+$imports
 
-${imports ?? '/*TODO: imports*/'}
+part '$fileName.g.dart';
 
-${fileName != null ? "part '$fileName.g.dart';" : "/*TODO: fileName*/"}
+''';
+    }
 
+    template += '''
 @JsonSerializable()
 class ${className ?? '/*TODO: className*/'} {
       ${className ?? '/*TODO: className*/'}();
 
   ${declaration ?? '/*TODO: declaration*/'}
 
-  factory ${className ?? '/*TODO: className*/'}.fromJson(Map<String,dynamic> json) => _\$${className ?? '/*TODO: className*/'}FromJson(json);
-  Map<String, dynamic> toJson() => _\$${className ?? '/*TODO: className*/'}ToJson(this);
-  
-${enumConverters}
-}
+  factory ${className ?? '/*TODO: className*/'}.fromJson(Map<String,dynamic> json) => _\$${className}FromJson(json);
+  Map<String, dynamic> toJson() => _\$${className}ToJson(this);
+''';
 
-${enums}
-""";
+    if ((enumConverters?.length ?? 0) > 0) {
+      template += '\n$enumConverters';
+    }
+
+    template += '}\n';
+
+    if ((enums?.length ?? 0) > 0) {
+      template += '\n$enums\n';
+    }
+
+    if ((nestedClasses?.length ?? 0) > 0) {
+      template += '\n$nestedClasses';
+    }
+
+    return template;
+  }
 
   static String indented(String content, {int indent}) {
     indent = indent ?? 1;
